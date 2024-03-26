ac0000d00b7b509b2678b2a3017e08462e1c7d19
DESKTOP-AGM34SA\seeti <asd>
2024-03-26 17:37:58 +0900

    user filter 추가

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Bot 2 청산된 주문 분석')
+
 
 # DataFrame 생성
 json_file_path = 'test.json'
@@ -12,6 +12,8 @@ df['timestamp'] = pd.to_datetime(df['timestamp'])
 # 시작 및 종료 timestamp 계산
 start_timestamp = df['timestamp'].min()
 end_timestamp = df['timestamp'].max()
+# 사용자 목록 생성 (중복 제거)
+user_list = df['user_id'].unique()
 
 
 def determine_profit_loss(group):
@@ -68,7 +70,15 @@ results = df.groupby('order_id').apply(aggregate_results, include_groups=False).
 profit_or_lost_result = results[(results["profit_loss"] == "loss") | (results["profit_loss"] == "profit")]
 
 if __name__=='__main__':
-
+    st.title('Bot 2 청산된 주문 분석')
+    # Streamlit selectbox를 사용하여 유저 선택
+    selected_user = st.selectbox('유저 선택:', user_list)
+    # 선택된 유저에 해당하는 데이터만 필터링
+    df = df[df['user_id'] == selected_user]
+    # apply 함수를 사용할 때 include_groups=False를 추가하거나 경고를 무시
+    results = df.groupby('order_id').apply(aggregate_results, include_groups=False).reset_index()
+    profit_or_lost_result = results[(results["profit_loss"] == "loss") | (results["profit_loss"] == "profit")]
+    
     st.dataframe(profit_or_lost_result, use_container_width=True)
     # 시간 범위 표시
     st.write(f"데이터 시간 범위: {start_timestamp} 부터 {end_timestamp} 까지")
