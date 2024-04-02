9a6a698e80d77e592b85906643c0d1ab621e1f4a
DESKTOP-AGM34SA\seeti <asd>
2024-04-02 15:56:52 +0900

    for page2

--- pages/all_logs.py
+++ pages/all_logs.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
+st.markdown("#all log ❄️")
+
+conn = st.connection('mysql', type='sql')
+# Perform query. 캐쉬 10분 설정 로직.
+df = conn.query('SELECT * from trade_logs;', ttl=3000)
+
+st.dataframe(df)
