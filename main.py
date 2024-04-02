3dba0262e7589944530af7f1287438582615f9d1
DESKTOP-AGM34SA\seeti <asd>
2024-04-02 16:22:50 +0900

    test

--- main.py
+++ main.py
@@ -70,8 +70,8 @@ if __name__ == '__main__':
     # Initialize connection.
     conn = st.connection('mysql', type='sql')
     # Perform query. 캐쉬 10분 설정 로직.
-    df = conn.query('SELECT * from trade_logs;', ttl=3000)
-
+    df = conn.query('SELECT * from trade_logs;')
+#ttl=3000
     # 유저 리스트 가져오기
     user_list = df['user_id'].unique()
     selected_user = st.selectbox('유저 선택:', user_list)
